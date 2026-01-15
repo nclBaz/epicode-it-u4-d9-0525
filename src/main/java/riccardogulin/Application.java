d9ef3dc2eb70cf2c6fb3fff0ba19f56961a62d48
Riccardo Gulin <riccardo.gulin@gmail.com>
2026-01-15 11:59:54 +0100

    Adds GroupingBy Examples

--- src/main/java/riccardogulin/Application.java
+++ src/main/java/riccardogulin/Application.java
@@ -28,9 +28,49 @@ public class Application {
 
 		System.out.println("*********************************** COLLECTORS *******************************************");
 
-		// 1. Raggruppiamo gli utenti per città
+		// 1. Raggruppiamo gli utenti minorenni per città
 		Map<String, List<User>> usersByCity = users.stream().filter(user -> user.getAge() < 18).collect(Collectors.groupingBy(user -> user.getCity()));
 		usersByCity.forEach((city, usersList) -> System.out.println("Citta: " + city + ", " + usersList));
+
+		// 2. Raggruppiamo gli utenti per età
+		Map<Integer, List<User>> usersByAge = users.stream().collect(Collectors.groupingBy(user -> user.getAge()));
+		usersByAge.forEach((age, usersList) -> System.out.println("Età: " + age + ", " + usersList));
+
+		// 3. Concateniamo tutti i nomi ed i cognomi in un'unica stringa: "Aldo Baglio. Giovanni Storti. Giacomo Poretti. ...."
+		String namesAndSurnames = users.stream().map(user -> user.getName() + " " + user.getSurname()).collect(Collectors.joining(". "));
+		System.out.println(namesAndSurnames);
+
+		// 4. Concateniamo tutte le età
+		String ages = users.stream().map(user -> "" + user.getAge()).collect(Collectors.joining(". "));
+		System.out.println(ages);
+
+		// 5. Calcolo della somma di tutte le età
+		int total = users.stream().collect(Collectors.summingInt(user -> user.getAge()));
+		System.out.println("La somma delle età è: " + total);
+
+		// 6. Calcolo la media delle età
+		double average = users.stream().collect(Collectors.averagingInt(user -> user.getAge()));
+		System.out.println("La media delle età è: " + average);
+
+		// 7. Raggruppiamo gli utenti per città e calcoliamo l'età media per ogni città
+		Map<String, Double> averageAgePerCity = users.stream()
+				.collect(
+						Collectors.groupingBy(
+								user -> user.getCity(),
+								Collectors.averagingInt(user -> user.getAge())
+						)
+				);
+		averageAgePerCity.forEach((city, averageAge) -> System.out.println("Città: " + city + ", " + averageAge));
+
+		// 8. Raggruppiamo gli utenti per città e calcoliamo tutta una serie di statistiche su quegli utenti, come età massima, età minima, media delle età....
+		Map<String, IntSummaryStatistics> statsPerCity = users.stream().collect(
+				Collectors.groupingBy(
+						user -> user.getCity(),
+						Collectors.summarizingInt(user -> user.getAge())
+				)
+		);
+
+		statsPerCity.forEach((city, stats) -> System.out.println("Città: " + city + ", " + stats));
 
 	}
 }
